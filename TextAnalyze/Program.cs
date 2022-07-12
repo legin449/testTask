223a88cd004a02ca628772ab36389f399393ac5c
legin449 <legin449@gmail.com>
2022-07-12 19:41:46 +0300

    initial commit

---
+++ TextAnalyze/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace Program
+{
+    class Program
+    {
+        //dict for triples
+        public static ConcurrentDictionary<string, int> triples = new ConcurrentDictionary<string, int>();
+        static List<Thread> threads = new List<Thread>();
+
+        static void Main()
+        {
+            //Init timer
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            //read text from txt
+            string TextTest = File.ReadAllText("book.txt").ToLower();
+
+            //replace all non-word chars to space
+            TextTest = Regex.Replace(TextTest, "\\W{1,}", " ");
+
+            //splitting text for multithreading
+            var spaces = Regex.Matches(TextTest, " ");
+            int spacesCount = spaces.Count();
+
+            //spiltBy could be changed for performance
+            int splitBy = 10;
+
+            //getting number of splits
+            int numOfProcesses = (spacesCount+splitBy) / splitBy;
+            int begin;
+            int end;
+
+            //getting text boundaries and starting proccess each part
+            for (int i = 0; i < spacesCount; i = i + numOfProcesses)
+            {
+                if(i + numOfProcesses < spacesCount)
+                {
+                    if(i==0)
+                    {
+                        begin = i;
+                        end = spaces[i + numOfProcesses].Index;
+                    }
+                    else
+                    {
+                        begin = spaces[i].Index;
+                        end = spaces[i + numOfProcesses].Index;
+                    }
+                }
+                else
+                {
+                    begin = spaces[i].Index;
+                    end = spaces[spacesCount - 1].Index;
+                }
+                if (!(begin == end))
+                {
+                    Thread thread = new Thread(() => FindTriplets(begin, end, TextTest));
+                    thread.Start();
+                    threads.Add(thread);
+                }
+            }
+            Console.WriteLine($"threads count: {threads.Count}");
+
+            //wait for all threads are closed
+            while (threads.Count(x => x.IsAlive) > 0)
+                Thread.Sleep(5);
+            //sorting dictionary
+            var triplesSorted = triples.OrderByDescending(key => key.Value);
+            int index = 0;
+            //displaying first 10 triples
+            foreach (var triple in triplesSorted)
+            {
+                Console.WriteLine($"{triple}");
+                index++;
+                if (index == 10)
+                {
+                    break;
+                }
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+
+        static void FindTriplets(int begining, int end, string Text)
+        {
+            for (int i = begining; i < end; i++)
+            {
+                string triple;
+                //if text is not ended
+                if (i + 2 <= end)
+                {
+                    //getting triple
+                    triple = Text[i].ToString() + Text[i + 1].ToString() + Text[i + 2].ToString();
+                    //if triple already in dict - skipping it
+                    if (triples.ContainsKey(triple))
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                //if space was faced in triple - skipping
+                if (triple.Contains(" "))
+                {
+                    continue;
+                }
+                //getting all matches for triple in text
+                var matches = Regex.Matches(Text, triple);
+                //adding triple to dict
+                if (matches.Count > 0)
+                {
+                    if (triples.ContainsKey(triple))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        triples[triple] = matches.Count();
+                    }
+                }
+            }
+        }
+    }
+}
